ab09d2106e03a3a82b9e66fb30e86df5335cd9a4
hndko <hd.doko22@gmail.com>
2023-10-26 21:35:56 +0700

    first upload

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert(
+            [
+                'name' => Str::random(10),
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin1234'),
+                'role' => 'admin'
+            ],
+            [
+                'name' => Str::random(10),
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('kasir1234'),
+                'role' => 'kasir'
+            ]
+        );
+    }
+}
